4b7e1d3fd9316db13c02bdb97776c2b99a632b6d
josephmiguel1103 <josephmiguel1103@gmail.com>
2023-11-29 17:17:25 -0500

    buscador incluido

--- routes/p1c.js
+++ routes/p1c.js
@@ -2,11 +2,13 @@ var express = require('express');
 var router = express.Router();
 var dbConn = require('../lib/db');
 
-/* listar. */
-router.get('/', function (req, res, next) {
+// Función para obtener todos los productos o realizar una búsqueda
+function getProducts(req, res, next, searchTerm = '') {
+  const query = searchTerm
+    ? 'SELECT * FROM productos WHERE nombre LIKE ? ORDER BY id_product ASC'
+    : 'SELECT * FROM productos ORDER BY id_product ASC';
 
-  dbConn.query('SELECT * FROM productos ORDER BY id asc', function (err, rows) {
-
+  dbConn.query(query, searchTerm ? [`%${searchTerm}%`] : [], function (err, rows) {
     if (err) {
       req.flash('error', err);
       res.render('productos', { data: '' });
@@ -14,7 +16,20 @@ router.get('/', function (req, res, next) {
       res.render('productos', { data: rows });
     }
   });
-}); 
+}
+
+// Ruta para listar todos los productos
+router.get('/', function (req, res, next) {
+  getProducts(req, res, next);
+});
+
+// Ruta para realizar busquueda
+router.get('/search', function (req, res, next) {
+  const searchTerm = req.query.q;
+  getProducts(req, res, next, searchTerm);
+});
+
+module.exports = router;
 
 
 
